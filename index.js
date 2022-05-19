83fe5efd8988055b6d5d9d36e697d1991bd78e30
TomaszBachorski <tomekbachorski@o2.pl>
2022-05-19 11:50:48 +0200

    api reach

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const fs = require("fs");
-const mysql = require("mysql");
+// const fs = require("fs");
+// const mysql = require("mysql");
 const bodyParser = require('body-parser')
 const cors = require("cors");
 const environment = require("../environment.json");
@@ -8,27 +8,16 @@ const port = environment.backEndPort || 7200;
 
 const app = express();
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
+app.use(cors());
 
-const router = express.Router()
-
-router.post("/register", register)
-
-app.listen(port, ()=>{
-    console.log(`Listening on port ${port}`)
+app.post("/register", (req, res) => {
+    console.log(req.body)
+    res.status(200).json({
+        "message": "SUCCESS"
+    });
 })
 
-function register(req, res, next) {
-    let connection = mysql.createConnection({
-        host: environment.mysqlHost,
-        user: environment.mysqlUser,
-        password: environment.mysqlPassword,
-        database: environment.mysqlDatabase
-    });
-
-    let insertionValues = [null, req.body.username, req.body.password, new Date(), req.body.email, new Date(req.body.birthdate), 0, "Translator", "default-1.png", req.body.languages.join(",")]
-    console.log(insertionValues)
-    connection.query(`INSERT INTO users VALUES (?)`, [insertionValues], (res, err)=>{
-        return
-    })
-}
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
