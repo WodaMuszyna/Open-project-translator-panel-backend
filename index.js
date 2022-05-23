3eaeeb1a626d2095c7846186c259e2d99f52732d
TomaszBachorski <tomekbachorski@o2.pl>
2022-05-23 13:31:34 +0200

    registration query

--- index.js
+++ index.js
@@ -37,11 +37,27 @@ app.get("/emailTaken/:email", (req, res) => {
     });
 })
 
-app.post("/register", (req, res) => {
-    res.status(200).json({
-        "message": "SUCCESS"
+app.post("/register", async (req, res) => {
+    let pass = await argon2.hash(req.body.password);
+    let insertionValues = [
+        null,
+        req.body.username,
+        req.body.surname,
+        req.body.name,
+        pass,
+        req.body.email,
+        req.body.birthdate.split("T")[0],
+        0,
+        "translator",
+        req.body.languages.join(",")
+    ]
+    connection.query(`INSERT INTO users(id, username, surname, name, password, email, birthdate, blocked, role, languages) VALUES (?)`, [insertionValues], (err, response)=>{
+        if (err) { res.status(500); res.end(); return; }
+        res.status(200).json({
+            message: "Success"
+        })
     });
-})
+});
 
 app.post("/login", (req, res) => {
     let typeOfLogin = "username";
