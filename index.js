684cc26605fb4d48c64556569f8ac4b6a6ce99b9
TomaszBachorski <tomekbachorski@o2.pl>
2022-06-09 11:22:10 +0200

    Ordering users by rank

--- index.js
+++ index.js
@@ -146,9 +146,9 @@ app.route("/authenticate").get((req, res) => {
 });
 
 app.post("/getUsers", (req, res)=>{
-    let andStatement = "";
-    if (req.body.language) andStatement=`WHERE languages like "%${req.body.language}%"`;
-    mysqlConnect().query(`SELECT username, rankId, languages FROM users ${andStatement};`, (err, response)=>{
+    let whereStatement = "";
+    if (req.body.language) whereStatement=`WHERE languages like "%${req.body.language}%"`;
+    mysqlConnect().query(`SELECT username, rankId, languages FROM users ${whereStatement} ORDER BY rankId DESC;`, (err, response)=>{
         if (err) { res.sendStatus(500); res.end(); return; };
         res.status(200).send(response).end();
     })
